253623253c50822b111e5f70eb5d0f5efe38220f
Ryan Cyrus <ryan.cyrus@live.com>
2017-08-13 17:50:04 -0400

    HW-04 Added HTMLCreator to handle html.

---
+++ HTMLWriter/HTMLCreator.h
@@ -0,0 +1,15 @@
+//
+//  HTMLCreator.h
+//  HTMLWriter
+//
+//  Created by Ryan on 2017-08-13.
+//  Copyright © 2017 Ryan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HTMLCreator : NSObject
+
+@property(nonatomic, strong)NSString* html;
+
+@end
---
+++ HTMLWriter/HTMLCreator.m
@@ -0,0 +1,22 @@
+//
+//  HTMLCreator.m
+//  HTMLWriter
+//
+//  Created by Ryan on 2017-08-13.
+//  Copyright © 2017 Ryan. All rights reserved.
+//
+
+#import "HTMLCreator.h"
+
+@implementation HTMLCreator
+
+-(id) init{
+    self = [super init];
+    if(self){
+        self.html =@"";
+        
+    }
+    return self;
+}
+
+@end
